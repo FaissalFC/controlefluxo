5128a8ac8b0cbe22045ab97471af7c087ba38ac6
FaissalFC <142908515+FaissalFC@users.noreply.github.com>
2023-10-03 17:01:27 -0300

    Add files via upload

---
+++ DesafioControleFluxo/src/ParametrosInvalidosException.java
@@ -0,0 +1,8 @@
+
+
+public class ParametrosInvalidosException extends Exception {
+    static String contar() throws ParametrosInvalidosException{
+          throw new ParametrosInvalidosException();
+
+    }
+}
